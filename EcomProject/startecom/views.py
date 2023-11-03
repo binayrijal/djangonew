9dbf7d5cc0a079a302a61d073648ce6fb2a44b6f
binayrijal <binayrijal22@gmail.com>
2023-11-03 09:30:38 +0545

    comments

--- EcomProject/startecom/views.py
+++ EcomProject/startecom/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'startecom/index.html')
+    return render(request,'startecom/index.html') #this is main home function to call
 
 
 def contact(request):
